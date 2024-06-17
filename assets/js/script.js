1b96389a7f69900a9c85479298709e41c864748a
Mitchell-610 <Mheit602@gmail.com>
2024-06-17 19:55:40 -0400

    If statement applied to make card to change color based off time now.

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,6 +45,12 @@ const cardDeleteBtn = $('<button>')
   if (task.dueDate && task.status !== 'done') {
     const now = dayjs();
     const taskDueDate = dayjs(task.dueDate, 'DD/MM/YYYY');
+    if (now.isSame(taskDueDate, 'day')) {
+      taskCard.addClass('bg-warning text-white');
+  } else if (now.isAfter(taskDueDate)) {
+      taskCard.addClass('bg-danger text-white');
+      cardDeleteBtn.addClass('border-light');
+  }
   }
 };
 
