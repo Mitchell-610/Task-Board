a158f22bdf8ce4d145f937988ea7955da7dd82d8
Mitchell-610 <Mheit602@gmail.com>
2024-06-19 21:25:04 -0400

    Tasks has updated value in handleDrop function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -160,7 +160,7 @@ function handleDeleteTask(event){
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
-  const tasks = taskList;
+  const tasks = readTasksFromStorage();
   const taskId = ui.draggable[0].dataset.taskId;
   const newStatus = event.target.id;
   for (let task of tasks) {
@@ -170,6 +170,7 @@ function handleDrop(event, ui) {
   }
   localStorage.setItem('tasks', JSON.stringify(tasks));
   renderTaskList();
+  console.log(`NG`)
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
